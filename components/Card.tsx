e2c6204ca5acd9dea6490c2194d2a23a04a21b67
sam <ironkwec@myumanitoba.ca>
2023-07-14 19:28:45 -0500

    s

--- components/Card.tsx
+++ components/Card.tsx
@@ -68,7 +68,15 @@ const Card = () => {
                 }
                 footer={
                   <div className="flex items-start justify-between flex-col sm:flex-row sm:items-center">
-                    P/L: N/A
+                    <button
+                       className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:[linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0D2247] no-underline flex gap-x-2  active:scale-95 scale-100 duration-75"
+                       style={{
+                         boxShadow:
+                           "0px 1px 4px rgba(13, 34, 71, 0.17), inset 0px 0px 0px 1px #061530, inset 0px 0px 0px 2px rgba(255, 255, 255, 0.1)",
+                       }}
+                     >
+                       BUY
+                     </button>
                   </div>
                 }
               >
