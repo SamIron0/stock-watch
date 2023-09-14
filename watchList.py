cecffa69fa4edc738490f44420edd792b2064ad7
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-14 14:15:53 -0500

    s

--- watchList.py
+++ watchList.py
@@ -1,6 +1,3 @@
-# this function will watch the stock throughout the day for when volume blows up. Store the volume throughtout the last hour-ish and check for unusual increase in volume
-
-# have a queue of volumes per minute
 
 volumes = [2000000, 3000000 ]
 print("watching TSLA")
