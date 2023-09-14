1afd53a252b3e35aefeaf863ccf4221baea3347f
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-14 23:02:50 +0000

    x

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,19 +5,17 @@ import { useRouter } from 'next/router';
 import Navbar from '@/components/ui/Navbar';
 import Footer from '@/components/ui/Footer';
 
-import { PageMeta } from '../types';
 
 interface Props extends PropsWithChildren {
-  meta?: PageMeta;
 }
 
-export default function Layout({ children, meta: pageMeta }: Props) {
+
+export default function Layout({ children }: Props) {
   const router = useRouter();
   const meta = {
     title: 'Trading bot',
-    description: 'Brought to you by Vercel, Stripe, and Supabase.',
-    cardImage: '/og.png',
-    ...pageMeta
+    description: 'Retrieve and display stock prices to screen',
+    
   };
 
   return (
@@ -34,12 +32,10 @@ export default function Layout({ children, meta: pageMeta }: Props) {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.cardImage} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@vercel" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.cardImage} />
       </Head>
       <Navbar />
       <main id="skip">{children}</main>
