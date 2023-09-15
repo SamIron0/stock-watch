63719c52d0f78b7bdc8534f6c20d408655c3e561
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-15 00:29:26 +0000

    o

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -3,14 +3,12 @@ import { useRouter } from 'next/router';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 import Logo from '@/components/icons/Logo';
-import { useUser } from '@/utils/useUser';
 
 import s from './Navbar.module.css';
 
 const Navbar = () => {
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
-  const { user } = useUser();
 
   return (
     <nav className={s.root}>
@@ -19,23 +17,9 @@ const Navbar = () => {
       </a>
       <div className="mx-auto max-w-6xl px-6">
         <div className="flex justify-between align-center flex-row py-4 md:py-6 relative">
- 
+
           <div className="flex flex-1 justify-end space-x-8">
-            {user ? (
-              <span
-                className={s.link}
-                onClick={async () => {
-                  await supabaseClient.auth.signOut();
-                  router.push('/signin');
-                }}
-              >
-                Sign out
-              </span>
-            ) : (
-              <Link href="/signin" className={s.link}>
-                Sign in
-              </Link>
-            )}
+
           </div>
         </div>
       </div>
