f3397240812e703d66c1be44edcdf6ebc5c7f03d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-15 00:27:20 +0000

    s

--- utils/useUser.tsx
+++ utils/useUser.tsx
@@ -5,12 +5,10 @@ import {
   User
 } from '@supabase/auth-helpers-react';
 
-import { UserDetails } from 'types';
 
 type UserContextType = {
   accessToken: string | null;
   user: User | null;
-  userDetails: UserDetails | null;
   isLoading: boolean;
 };
 
@@ -21,47 +19,6 @@ export const UserContext = createContext<UserContextType | undefined>(
 export interface Props {
   [propName: string]: any;
 }
-
-export const MyUserContextProvider = (props: Props) => {
-  const {
-    session,
-    isLoading: isLoadingUser,
-    supabaseClient: supabase
-  } = useSessionContext();
-  const user = useSupaUser();
-  const accessToken = session?.access_token ?? null;
-  const [isLoadingData, setIsloadingData] = useState(false);
-  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-
-  const getUserDetails = () => supabase.from('users').select('*').single();
-  
-  useEffect(() => {
-    if (user && !isLoadingData && !userDetails) {
-      setIsloadingData(true);
-      Promise.allSettled([getUserDetails()]).then(
-        (results) => {
-          const userDetailsPromise = results[0];
-
-          if (userDetailsPromise.status === 'fulfilled')
-            setUserDetails(userDetailsPromise.value.data as UserDetails);
-
-          setIsloadingData(false);
-        }
-      );
-    } else if (!user && !isLoadingUser && !isLoadingData) {
-      setUserDetails(null);
-    }
-  }, [user, isLoadingUser]);
-
-  const value = {
-    accessToken,
-    user,
-    userDetails,
-    isLoading: isLoadingUser || isLoadingData,
-  };
-
-  return <UserContext.Provider value={value} {...props} />;
-};
 
 export const useUser = () => {
   const context = useContext(UserContext);
